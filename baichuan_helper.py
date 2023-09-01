2856c2517532494aef636a0c3a760202fff7de62
qianshuang <2009qianshuang@163.com>
2023-09-01 10:01:33 +0800

    update

--- baichuan_helper.py
+++ baichuan_helper.py
@@ -4,9 +4,10 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.generation.utils import GenerationConfig
 
-tokenizer = AutoTokenizer.from_pretrained("baichuan-inc/Baichuan-13B-Chat", use_fast=False, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("baichuan-inc/Baichuan-13B-Chat", device_map="auto", torch_dtype=torch.float16, trust_remote_code=True)
-model.generation_config = GenerationConfig.from_pretrained("baichuan-inc/Baichuan-13B-Chat")
+model_path = "baichuan-inc/Baichuan-13B-Chat"
+tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto", torch_dtype=torch.float16, trust_remote_code=True)
+model.generation_config = GenerationConfig.from_pretrained(model_path)
 
 
 def get_chatyuan_res(messages):
