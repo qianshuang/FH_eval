bab5b1c0709cf764ebe11586e39514a504b84e85
qianshuang <2009qianshuang@163.com>
2023-09-01 10:08:51 +0800

    update

--- test.py
+++ test.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import torch
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import AutoModelForSequenceClassification
 
-tokenizer = T5Tokenizer.from_pretrained("ClueAI/ChatYuan-large-v2")
-model = T5ForConditionalGeneration.from_pretrained("ClueAI/ChatYuan-large-v2").half()
-device = torch.device('cuda')
-model.to(device)
-
-# text = f"{context}\n用户：{text}\n小元："
-text = "你是谁？"
-encoding = tokenizer(text=[text], truncation=True, padding=True, max_length=1024, return_tensors="pt").to(device)
-out = model.generate(**encoding, return_dict_in_generate=True, output_scores=False, max_new_tokens=1024, do_sample=True, top_p=1, temperature=0, no_repeat_ngram_size=12)
-out_text = tokenizer.batch_decode(out["sequences"], skip_special_tokens=True)
-print(out_text[0])
+model_name = "xlm-roberta-base"
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
